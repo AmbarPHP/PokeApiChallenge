5f282c6e025c31d5caa3cc1b9d0b317ee5269522
AmbarPHP <perlamarina.franco@gmail.com>
2024-01-05 14:48:09 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+
+function App() {
+  const routes = [
+    { path: "/", component: <Home /> },
+
+
+  ];
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route, index) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.path) {
+        return (
+          <Route
+            path={route.path}
+            element={route.component}
+            key={index}
+          />
+        );
+      }
+
+      return null;
+    });
+
+  return (
+    <div className="App">
+
+      <div className="main">
+
+        <Router>
+          <Routes>{getRoutes(routes)}</Routes>
+        </Router>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
