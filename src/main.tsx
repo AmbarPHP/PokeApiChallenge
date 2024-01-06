ee6b155b061817b531606283143dcbde9b3aec06
AmbarPHP <perlamarina.franco@gmail.com>
2024-01-06 07:27:26 -0700

    add pokemon slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-/* import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store'; */
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App.tsx'
-/* import './index.css' */
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-  
-      </PersistGate>
-    </Provider> */}
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
 
   </React.StrictMode>,
 )
