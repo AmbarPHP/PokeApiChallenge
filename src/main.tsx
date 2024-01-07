d474f24b0d2e28c7216b1a41bbfcd86a5b8c8ca8
AmbarPHP <perlamarina.franco@gmail.com>
2024-01-07 08:45:49 -0700

    add store

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-/* import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store'; */
-import App from './App.tsx'
-/* import './index.css' */
+import { Provider } from 'react-redux';
+import store from "./redux/store";
+import App from './App.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/*     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-  
-      </PersistGate>
-    </Provider> */}
-    <App />
+    <React.StrictMode>
 
-  </React.StrictMode>,
+        <Provider store={store}>
+            <App />
+        </Provider>
+
+    </React.StrictMode>,
 )
