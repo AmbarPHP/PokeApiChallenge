f9ee857e7f2ee8141097bd1295198bc3405742a0
AmbarPHP <perlamarina.franco@gmail.com>
2024-01-07 14:59:43 -0700

    add default Imagen and  format style

--- src/components/PokemonImagen.tsx
+++ src/components/PokemonImagen.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 
 
 function PokemonImagen() {
+    //obtner imagen
+    const [imagen, setImagen] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/65.png");
 
     return (
-        <div>PokemonImagen</div>
+        <div>
+            <img src={imagen} alt="" className="img-fluid " style={{ height: "100vh" }} />
+        </div>
     )
 }
 
