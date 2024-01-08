6617d7ed0a9235b784b053451e2cf14de10fd324
AmbarPHP <perlamarina.franco@gmail.com>
2024-01-07 17:55:44 -0700

    trying to read data from store

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,43 @@
 
 import { Button } from 'react-bootstrap'
-import { IPokemon } from '../redux/slices/pokemon.interface'
+import { IPokemon } from '../redux/slices/pokemon.interface';
+import { setCurrentPokemonUrl } from "../redux/slices/pokemonImagenSlice";
+import { AppDispatch } from "../redux/store";
+import { useDispatch } from "react-redux";
+
 interface IParams {
     event: Event;
     url: string
 }
 interface IPokemonCard {
     data: IPokemon;
-    handleClick: ({ event, url }: IParams) => void;
 }
 
-function PokemonCard({ data, handleClick }: IPokemonCard) {
+
+
+function PokemonCard({ data }: IPokemonCard) {
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleClick = (event: { detail: any; } | undefined, url: string) => {
+        console.log(event?.detail, url);
+
+
+        switch (event?.detail) {
+            case 1: {
+                console.log('single click');
+                dispatch(setCurrentPokemonUrl(url))
+                break;
+            }
+            case 2: {
+                console.log('double click');
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    };
     return (
         <Button className="btn  btn-primary" onClick={(event) => handleClick(event, data.url)} >
             {data.name}
